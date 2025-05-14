aad0c52f9560dd30194a0499af77b7524b4ac060
SpaceCatNugget <81484715+SpaceCatNugget@users.noreply.github.com>
2025-05-14 17:07:52 +0300

    Built a responsive layout

--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,12 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { ChakraProvider } from "@chakra-ui/react";
-import { system } from "@chakra-ui/react/preset";
 import "./index.css";
 import App from "./App.tsx";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <ChakraProvider value={system}>
+    <ChakraProvider>
       <App />
     </ChakraProvider>
   </StrictMode>
