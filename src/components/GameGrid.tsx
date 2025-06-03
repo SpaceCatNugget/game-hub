e1d3aad9413b040295d04d772b35d72376619edb
SpaceCatNugget <81484715+SpaceCatNugget@users.noreply.github.com>
2025-06-03 17:32:35 +0300

    Building game cards.

--- src/components/GameGrid.tsx
+++ src/components/GameGrid.tsx
@@ -1,16 +1,21 @@
-import { Text } from "@chakra-ui/react";
+import { Text, SimpleGrid } from "@chakra-ui/react";
 import useGames from "@/hooks/useGames";
+import GameCard from "./GameCard";
 
 const GameGrid = () => {
   const { games, error } = useGames();
   return (
     <>
       {error && <Text>{error}</Text>}
-      <ul>
+      <SimpleGrid
+        columns={{ sm: 1, md: 2, lg: 3, xl: 5 }}
+        padding={10}
+        spacing={10}
+      >
         {games.map((games) => (
-          <li key={games.id}>{games.name}</li>
+          <GameCard key={games.id} game={games}></GameCard>
         ))}
-      </ul>
+      </SimpleGrid>
     </>
   );
 };
